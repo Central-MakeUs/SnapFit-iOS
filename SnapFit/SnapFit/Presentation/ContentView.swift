4183527741cd233eed3d62053e4b4fc798dabf54
jeonguk29 <jeonguk29@naver.com>
2024-07-09 18:31:16 +0900

    style : project Clean Architecture Setting

---
+++ SnapFit/SnapFit/Presentation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapFit
+//
+//  Created by 정정욱 on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapFit/SnapFit/SnapFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapFitApp.swift
+//  SnapFit
+//
+//  Created by 정정욱 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
