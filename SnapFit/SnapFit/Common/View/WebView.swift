34a36e45b27b3f3063f59604c35bc85dac7d00e3
jeonguk29 <jeonguk29@naver.com>
2024-07-16 14:50:39 +0900

    feat : 서비스 이용약관, 닉네임 설정 View 구현

---
+++ SnapFit/SnapFit/Common/View/WebView.swift
@@ -0,0 +1,22 @@
+//
+//  WebView.swift
+//  SnapFit
+//
+//  Created by 정정욱 on 7/15/24.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
